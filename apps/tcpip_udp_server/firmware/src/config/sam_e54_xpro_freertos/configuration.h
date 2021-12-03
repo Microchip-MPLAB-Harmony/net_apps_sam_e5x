a9f06f6b4389ba78edf15957aa8e5dd617e23a63
Niyas Hameed <niyas.hameed@microchip.com>
2021-12-03 11:47:00 +0530

    MH3-60983	tcpip_udp_server application regeneration - SAME54 MH3-60984	tcpip_udp_server application regeneration- SAME54_freertos

--- apps/tcpip_udp_server/firmware/src/config/sam_e54_xpro_freertos/configuration.h
+++ apps/tcpip_udp_server/firmware/src/config/sam_e54_xpro_freertos/configuration.h
@@ -56,7 +56,7 @@
 */
 
 #include "user.h"
-#include "toolchain_specifics.h"
+#include "device.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -159,8 +159,7 @@ extern "C" {
 #define TCPIP_DNS_CLIENT_CACHE_PER_IPV6_ADDRESS		1
 #define TCPIP_DNS_CLIENT_ADDRESS_TYPE			    IP_ADDRESS_TYPE_IPV4
 #define TCPIP_DNS_CLIENT_CACHE_DEFAULT_TTL_VAL		1200
-#define TCPIP_DNS_CLIENT_CACHE_UNSOLVED_ENTRY_TMO	10
-#define TCPIP_DNS_CLIENT_LOOKUP_RETRY_TMO			5
+#define TCPIP_DNS_CLIENT_LOOKUP_RETRY_TMO			2
 #define TCPIP_DNS_CLIENT_MAX_HOSTNAME_LEN			64
 #define TCPIP_DNS_CLIENT_MAX_SELECT_INTERFACES		4
 #define TCPIP_DNS_CLIENT_DELETE_OLD_ENTRIES			true
@@ -327,13 +326,13 @@ extern "C" {
 
 		/*** QUEUE 0 TX Configuration ***/
 #define TCPIP_GMAC_TX_DESCRIPTORS_COUNT_QUE0				8
-#define TCPIP_GMAC_TX_BUFF_SIZE_QUE0				    	1536			
 #define TCPIP_GMAC_MAX_TX_PKT_SIZE_QUE0				    	1536
 		
 		/*** QUEUE 0 RX Configuration ***/
 #define TCPIP_GMAC_RX_DESCRIPTORS_COUNT_QUE0				8
 #define TCPIP_GMAC_RX_BUFF_SIZE_QUE0				    	1536
-#define TCPIP_GMAC_RX_BUFF_COUNT_QUE0				   		10
+#define TCPIP_GMAC_RX_DEDICATED_BUFFERS_QUE0				8
+#define TCPIP_GMAC_RX_ADDL_BUFF_COUNT_QUE0				   	2
 #define TCPIP_GMAC_RX_BUFF_COUNT_THRESHOLD_QUE0			1
 #define TCPIP_GMAC_RX_BUFF_ALLOC_COUNT_QUE0				2
 
@@ -373,7 +372,7 @@ extern "C" {
 #define TCPIP_GMAC_TX_PRIO_COUNT				1
 #define TCPIP_GMAC_RX_PRIO_COUNT				1
 #define DRV_GMAC_NUMBER_OF_QUEUES				1
-#define DRV_GMAC_RMII_MODE					0
+#define DRV_GMAC_RMII_MODE						0
 
 
 
@@ -387,6 +386,9 @@ extern "C" {
 #define TCPIP_UDP_USE_RX_CHECKSUM             			true
 #define TCPIP_UDP_COMMANDS   false
 #define TCPIP_UDP_EXTERN_PACKET_PROCESS   false
+
+
+#define TCPIP_STACK_NETWORK_INTERAFCE_COUNT  	1
 
 
 
