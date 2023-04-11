c58fbe8edbac41e4e60c4e148df78019522e6f72
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-04-11 11:13:33 +0530

    [MH3-75423] tcpip_tcp_client application regeneration- SAME54

--- apps/tcpip_tcp_client/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/tcpip_tcp_client/firmware/src/config/sam_e54_xpro/definitions.h
@@ -60,8 +60,8 @@
 #include "system/reset/sys_reset.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "peripheral/sercom/usart/plib_sercom2_usart.h"
 #include "driver/gmac/drv_gmac.h"
+#include "peripheral/sercom/usart/plib_sercom2_usart.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "system/command/sys_command.h"
 #include "peripheral/port/plib_port.h"
@@ -202,8 +202,8 @@ typedef struct
 
 
     SYS_MODULE_OBJ  tcpip;
+   SYS_MODULE_OBJ  drvMiim_0;
 
-    SYS_MODULE_OBJ  drvMiim;
     SYS_MODULE_OBJ  sysDebug;
 
 
