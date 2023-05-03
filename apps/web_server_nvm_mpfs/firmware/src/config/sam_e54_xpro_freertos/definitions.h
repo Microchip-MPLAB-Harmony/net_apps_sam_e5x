e9b4ebefef2c3d893c163a796a8a1157e2ae4c28
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-05-03 21:17:47 +0530

    [MH3-79979] web_server_nvm_mpfs application regeneration- SAME54_freertos

--- apps/web_server_nvm_mpfs/firmware/src/config/sam_e54_xpro_freertos/definitions.h
+++ apps/web_server_nvm_mpfs/firmware/src/config/sam_e54_xpro_freertos/definitions.h
@@ -56,7 +56,6 @@
 #include "driver/memory/drv_memory_nvmctrl.h"
 #include "bsp/bsp.h"
 #include "system/int/sys_int.h"
-#include "system/ports/sys_ports.h"
 #include "system/cache/sys_cache.h"
 #include "system/reset/sys_reset.h"
 #include "osal/osal.h"
@@ -64,8 +63,8 @@
 #include "library/tcpip/tcpip.h"
 #include "system/sys_time_h2_adapter.h"
 #include "system/sys_random_h2_adapter.h"
-#include "peripheral/sercom/usart/plib_sercom2_usart.h"
 #include "driver/gmac/drv_gmac.h"
+#include "peripheral/sercom/usart/plib_sercom2_usart.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "system/command/sys_command.h"
 #include "peripheral/port/plib_port.h"
@@ -217,8 +216,8 @@ typedef struct
 
 
     SYS_MODULE_OBJ  tcpip;
+   SYS_MODULE_OBJ  drvMiim_0;
 
-    SYS_MODULE_OBJ  drvMiim;
     SYS_MODULE_OBJ  sysDebug;
 
     SYS_MODULE_OBJ  netPres;
