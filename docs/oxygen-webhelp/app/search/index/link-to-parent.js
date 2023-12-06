aee8a546c68a37b8d0f6bcd191eb7ad8780747fd
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-12-06 21:30:18 +0530

    [MH3-87386] - Updated the demo documentation for SAME54

---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {32:2,28:2,0:2,19:2,25:2,2:-1,34:2,35:-1,26:35,1:2,18:35,3:2,6:2};
+});
