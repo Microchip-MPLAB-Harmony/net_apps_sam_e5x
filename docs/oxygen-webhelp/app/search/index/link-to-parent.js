567d17394163fe5015e48d267c2a5dcfa26581c4
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2025-02-10 10:57:13 +0530

    [MH3-110166] - Documentation improvements for net_apps_sam_e5x

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++
@@ -1,4 +0,0 @@
-/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-define(function () {
-return {29:2,33:2,20:2,2:-1,35:2,19:36,36:-1,0:2,27:36,6:2,3:2,26:2,1:2};
-});
